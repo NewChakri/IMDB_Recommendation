e5902c9989405edd3f9edfbd1b8930df3ac92f4d
NewChakri <99199609+NewChakri@users.noreply.github.com>
2024-06-14 12:49:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,10 +15,9 @@ favorite_genres = st.multiselect('Select favorite genres:', genres_list)
 min_year = st.slider('Select minimum year:', min_value=1900, max_value=2025, value=2010)
 max_year = st.slider('Select maximum year:', min_value=1900, max_value=2025, value=2025)
 allowed_ratings = st.multiselect('Select allowed ratings:', movies_df['Rate'].unique())
-min_rating = st.slider('Select minimum rating:', min_value=0.0, max_value=10.0, value=7.0)
 
 if favorite_genres and allowed_ratings:
-    recommended_movies = recommend_movies(movies_df, favorite_genres, min_rating=min_rating, min_year=min_year, max_year=max_year, allowed_ratings=allowed_ratings)
+    recommended_movies = recommend_movies(movies_df, favorite_genres, min_rating=None, min_year=min_year, max_year=max_year, allowed_ratings=allowed_ratings)
     st.write(f"Recommendations for your preferences:")
 
     for idx, row in recommended_movies.iterrows():
