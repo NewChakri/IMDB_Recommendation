3c7d38ad3eb3b2bf47161016cbd2821babfe7d82
NewChakri <99199609+NewChakri@users.noreply.github.com>
2024-06-14 10:28:07 +0700

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
 def recommend_movies(df, favorite_genres, min_rating=None, min_year=None, max_year=None, allowed_ratings=None):
+    # Preprocess data
+    df['Rating'] = df['Rating'].replace('N/A', 0).astype(float)
+    df['Year'] = df['Year'].replace('N/A', 0).astype(int)
+    df['Rate'] = df['Rate'].fillna('Unrated')
     # Filter movies based on favorite genres, minimum rating, year range, and allowed ratings
     filtered_movies = df[
         df['Genres'].apply(lambda x: any(genre in x for genre in favorite_genres)) &
@@ -13,4 +17,4 @@ def recommend_movies(df, favorite_genres, min_rating=None, min_year=None, max_ye
     # Sort filtered movies by rating in descending order
     recommended_movies = filtered_movies.sort_values(by='Rating', ascending=False)
 
-    return recommended_movies[['Name', 'Year', 'Rating', 'Rate', 'Genres', 'Duration','Image_URL','URL']]
+    return recommended_movies[['Name', 'Year', 'Rating', 'Rate', 'Genres', 'Duration','Image_URL', 'URL']]
